2ca80f9548d355997a9779211ede264d9fb26ad1
fuiyiqi@gmail.com <fuiyiqi@gmail.com>
2025-03-10 20:33:24 +0800

    formatted app.js to become more pretty

--- App.js
+++ App.js
@@ -1,42 +1,38 @@
-import React,{Component}  from "react";
+import React, {Component} from 'react';
 import {StyleSheet} from 'react-native';
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-import HomeScreen from "./screen/HomeScreen.js";
-import AnimalScreen from "./screen/AnimalScreen.js";
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import HomeScreen from './screen/HomeScreen.js';
+import AnimalScreen from './screen/AnimalScreen.js';
 
-  const Stack = createStackNavigator();
+const Stack = createStackNavigator();
 
 //export default class App extends Component{
 const App = () => {
-    return(
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen 
-            name="Home" 
-            component={HomeScreen} 
-            options={{title:'HomeScreen', ...styles}}/>
-          <Stack.Screen 
-            name="Animal" 
-            component={AnimalScreen} 
-            options={styles}/>
-        </Stack.Navigator>
-      </NavigationContainer>
-    )
-  }
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{title: 'HomeScreen', ...styles}}
+        />
+        <Stack.Screen name="Animal" component={AnimalScreen} options={styles} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
 
-  
+const styles = StyleSheet.create({
+  headerTitleAlign: 'center',
+  headerStyle: {
+    backgroundColor: '#f4511e',
+  },
+  headerTintColor: '#ffffffff',
+  headerTitleStyle: {
+    //fontWeight: 'bold',
+    fontFamily: 'Anta-Regular',
+  },
+});
 
-  const styles = StyleSheet.create({
-    headerTitleAlign:'center',
-    headerStyle: {
-        backgroundColor: '#f4511e',
-      },
-    headerTintColor: '#ffffffff',
-    headerTitleStyle: {
-      //fontWeight: 'bold',
-      fontFamily: 'Anta-Regular',
-    }
-  })
-
-  export default App;
+export default App;
